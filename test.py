dd16fb6a7bb65777f137fa49eddc6f180fd157c2
Genie Park <genie0320@gmail.com>
2024-01-20 12:30:21 +0900

    _test_update

--- test.py
+++ test.py
@@ -1,36 +1,20 @@
-# %% [markdown]
-# # 공통부분
-# 목록 > 상세로 가면서 어차피 반복적으로 호출하게 될 코드를 함수로 만들었다.
-
-# %%
-from bs4 import BeautifulSoup
 import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
+import random
 
-def go_to_page(url: str): 
+def go_to_page(url, head_info): #by requests
     URL = url
-    response = requests.get(URL)
+    HEADERS = head_info
+    response = requests.get(URL, timeout=5, headers=HEADERS)
     soup = BeautifulSoup(response.content, "html.parser", from_encoding='utf-8')
-    return [response.status_code, soup]
+    return [response.status_code, URL, soup, f'type: {type(soup)}']
 
-# %%
-start_url = 'https://books.toscrape.com/index.html'
-root_url = 'https://books.toscrape.com/' # 상세페이지 url이 상대경로인 경우 입력.
-
-soup = go_to_page(start_url)
-link_tags = soup[1].find_all('h3', limit=3)
-links = []
-
-# 
-for link in link_tags:
-    link_ = link.find('a')['href']
-    links.append(root_url+link_)
-
-for url in links:
-    soup_detail = go_to_page(url)
-    title = soup_detail[1].h1.text
-    desc_tag = soup_detail[1].find('p', class_='')
-    desc = desc_tag.text
-
-    print(title)
-    print(desc)
-    print('*******************')
+def get_page_html(url, *args):
+    result = []
+    driver = webdriver.Chrome()
+    driver.get(url)
+    driver.implicitly_wait(10)  # 암시적 대기
